98bddee0f0f36c6dae1a360e432494a3bfa30abb
Fawwaz Syiham Muyassar <fawwazsyihamm@gmail.com>
2020-12-08 10:35:04 +0700

    Add menu loop

--- lib/ui/restaurant_detail.dart
+++ lib/ui/restaurant_detail.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:restaurant_app/models/models.dart';
 
@@ -8,14 +10,15 @@ class RestaurantDetailPage extends StatelessWidget {
   final Food food;
   final Drink drinks;
 
-  const RestaurantDetailPage({@required this.restaurants, this.food, this.drinks});
+  const RestaurantDetailPage(
+      {@required this.restaurants, this.food, this.drinks});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: NestedScrollView(
-        headerSliverBuilder: (context, isScrolled){
-          return[
+        headerSliverBuilder: (context, isScrolled) {
+          return [
             SliverAppBar(
               pinned: true,
               expandedHeight: 200,
@@ -32,87 +35,76 @@ class RestaurantDetailPage extends StatelessWidget {
             )
           ];
         },
-        body: ListView(
-          padding: const EdgeInsets.all(16),
-          children:[
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  restaurants.name,
-                  style: Theme.of(context).textTheme.headline4,
-                ),
-                SizedBox(height: 8),
-                Row(
-                  children: [
-                    Icon(
-                      Icons.location_on
-                    ),
-                    SizedBox(width: 8),
-                    Text(
-                      restaurants.city,
-                      style: Theme.of(context).textTheme.subtitle1,
-                    ),
-                  ],
-                ),
-                SizedBox(height: 8),
-                Text(
-                  'About',
-                  style: Theme.of(context).textTheme.headline6,
-                ),
-                SizedBox(height: 8),
-                Text(restaurants.description),
-                SizedBox(height: 8),
-                Text(
-                  'Menu',
-                  style: Theme.of(context).textTheme.headline6,
-                ),
-                SizedBox(height: 16),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Container(
-                      padding: const EdgeInsets.only(top: 60, left: 8),
-                      height: 100,
-                      width: 160,
-                      decoration: BoxDecoration(
+        body: ListView(padding: const EdgeInsets.all(16), children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                restaurants.name,
+                style: Theme.of(context).textTheme.headline4,
+              ),
+              SizedBox(height: 8),
+              Row(
+                children: [
+                  Icon(Icons.location_on),
+                  SizedBox(width: 8),
+                  Text(
+                    restaurants.city,
+                    style: Theme.of(context).textTheme.subtitle1,
+                  ),
+                ],
+              ),
+              SizedBox(height: 8),
+              Text(
+                'About',
+                style: Theme.of(context).textTheme.headline6,
+              ),
+              SizedBox(height: 8),
+              Text(restaurants.description),
+              SizedBox(height: 8),
+              Text(
+                'Menu',
+                style: Theme.of(context).textTheme.headline6,
+              ),
+              SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.only(top: 20, left: 8),
+                    height: restaurants.menus.foods.length * 25.0,
+                    width: 160,
+                    decoration: BoxDecoration(
                         color: Colors.grey,
-                        borderRadius: BorderRadius.circular(10)
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            '$food.name',
-                            style: Theme.of(context).textTheme.headline5,
-                          )
-                        ],
-                      ),
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        for (var item in restaurants.menus.foods)
+                          Text(item.name)
+                      ],
                     ),
-                    Container(
-                      padding: const EdgeInsets.only(top: 60, left: 8),
-                      height: 100,
-                      width: 160,
-                      decoration: BoxDecoration(
+                  ),
+                  Container(
+                    padding: const EdgeInsets.only(top: 20, left: 8),
+                    height: restaurants.menus.drinks.length * 25.0,
+                    width: 160,
+                    decoration: BoxDecoration(
                         color: Colors.grey,
-                        borderRadius: BorderRadius.circular(10)
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            '$drinks.name',
-                            style: Theme.of(context).textTheme.headline5,
-                          )
-                        ],
-                      ),
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        for (var item in restaurants.menus.drinks)
+                          Text(item.name)
+                      ],
                     ),
-                  ],
-                )
-              ],
-            ),
-          ]
-        ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ]),
       ),
     );
   }
